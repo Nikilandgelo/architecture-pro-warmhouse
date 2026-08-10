e3c93ed68caea04b6d8fdad1ed26d6bf26f57f67
Nikita Selivanov <flgjkdoged228@gmail.com>
2026-08-10 04:33:09 +0300

    refactor: remove aliases in telemetry payload

--- microservices/telemetry/src/telemetry/entities/telemetry.py
+++ microservices/telemetry/src/telemetry/entities/telemetry.py
@@ -6,8 +6,8 @@ from pydantic import BaseModel, Field
 
 
 class TelemetryPayload(BaseModel):
-    device_serial_number: str = Field(alias="serial_number")
-    device_location: str = Field(alias="location")
+    device_serial_number: str
+    device_location: str
     metrics: dict[str, float] = Field(default_factory=dict)
     states: dict[str, str] = Field(default_factory=dict)
 
